26a025666d726e1db149853e25e7983a50e7a5a1
CDXRACI <phuccd.icv@gmail.com>
2024-10-20 07:26:25 +0700

    FM_V0x_Farming commit with timestamp is: 20Sun10

--- autoCmm.bat
+++ autoCmm.bat
@@ -33,7 +33,7 @@ set   Flag_git_Push   = %F_FALSE%
 set   Flag_git_pull   = %F_FALSE%
 echo *============/Loading: 40% define hastag commit------------------*
 REM define hastag 
-set hihihehe=FM_V0x_Farming 
+
 echo *===============/Loading: 50% define directory-------------------*
 REM Define Directory
 set Directory=C:\00_WorkSpace\FM_Master\FM_V03_01\FM_V03_01
@@ -79,9 +79,7 @@ REM define functions
 			set day=%%b
 		)
 		set timestamp=%year%%month%%day%
-		echo %hihihehe%
-		set hohihi=%hihihehe% %timestamp%
-		git commit -m %hihihehe%
+		git commit -m "FM_V0x_Farming commit with timestamp is: %timestamp%"
 		echo Add files to repository.
 	) else (
 		echo Failed to add files to repository. )
